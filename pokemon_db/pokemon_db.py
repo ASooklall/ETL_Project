92af4a57d50dea8d30bc4fc20ff221285f063c09
Andrew Sooklall <andrewsooklall@gmail.com>
2019-11-07 18:33:28 -0500

    updated to version 2.0

--- pokemon_db/pokemon_db.py
+++ pokemon_db/pokemon_db.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
 from config import password
+import pprint as pp
 
 
 # Set locations for CSV Files and create dataframes
@@ -23,21 +24,11 @@ engine = create_engine(f'postgresql://{rds_connection_string}')
 ## Get Table Names
 # engine.table_names()
 
+
 # Read Dataframes into SQL and replace table if exists
 pokedex_df.to_sql(name='pokedex', con=engine, if_exists='replace', index=False)
 trainer_df.to_sql(name='trainer', con=engine, if_exists='replace', index=False)
 trainer_junction_df.to_sql(name='trainer_junction', con=engine, if_exists='replace', index=False)
-
-
-## Search Queries Test
-
-# x = pd.read_sql_query('select * from pokedex LIMIT 20', con=engine)
-# y = pd.read_sql_query('SELECT COUNT (*) FROM trainer', con=engine)
-# z = pd.read_sql_query('select * from trainer_junction LIMIT 20', con=engine)
-# print(x)
-# print(y)
-# print(z)
-
 
 
 # Search Function
@@ -60,17 +51,33 @@ def search():
         \nMenu Number: ")
 
     if request == 1 or request == str(1):
-        print('1')
+        trainer_search = input('Search for a trainer using their name:\nTrainer Name: ')
+        search_return1 = pd.read_sql_query(\
+            "SELECT tr.trainer_id, tr.trainername, t_j.pokelevel, pk.pokemon_name\
+                FROM trainer_junction as t_j \
+                LEFT JOIN trainer AS tr ON tr.trainer_id = t_j.trainer_id \
+                LEFT JOIN pokedex as pk ON t_j.pokedex_number = pk.pokedex_number \
+                WHERE tr.trainername LIKE " "'%%" + str((trainer_search).title()) + "%%'", con=engine) 
+        print(search_return1)
         resume_search()
 
     elif request == 2 or request == str(2):
-        print ('2')
+        poke_search = input('For a list of all trainers owning this pokemon: \nPokemon Name: ')
+        search_return2 = pd.read_sql_query(\
+            "SELECT tr.trainername\
+                FROM trainer_junction as t_j \
+                LEFT JOIN trainer AS tr ON tr.trainer_id = t_j.trainer_id \
+                LEFT JOIN pokedex as pk ON t_j.pokedex_number = pk.pokedex_number \
+                WHERE pk.pokemon_name = " "'" + str((poke_search).title()) + "'", con=engine) 
+        search_result2 = [val[0] for val in search_return2.values]
+        pp.pprint(search_result2)
         resume_search()
 
     elif request == 3 or request == str(3):
         poke_name = input('What is the name of the pokemon whose pokedex entry you wish to search for? \nPokemon Name: ')
         search_return3 = pd.read_sql_query("SELECT * FROM pokedex WHERE pokemon_name = " + "'" + str((poke_name).title()) + "'", con=engine)
-        print(search_return3)
+        search_result3 = search_return3.transpose()
+        print(search_result3)
         resume_search()
 
     elif request == 4 or request == str(4):
@@ -78,7 +85,8 @@ def search():
             \nPlease note that we have our own pokedex numbering system. \
             \nPokedex Number: ')
         search_return4 = pd.read_sql_query("SELECT * FROM pokedex WHERE pokedex_number = " + "'" + str(poke_num) + "'", con=engine)
-        print(search_return4)
+        search_result4 = search_return4.transpose()
+        print(search_result4)
         resume_search()
 
     elif request == 0 or request == str(0):
